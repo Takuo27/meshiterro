7c5e32c613e4270166453c08d0517a86a6084585
Takuo27 <103346206+Takuo27@users.noreply.github.com>
2022-07-09 01:26:29 +0000

    [Finish] 5-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -19,4 +20,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
 end
